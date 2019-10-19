390638bd2f50d4c04019bdab237519dc37e5fe57
Agung Priambada <apriambada@yahoo.co.id>
2019-10-19 15:44:12 +0800

    Update stylish-portfolio.js

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -65,5 +65,5 @@ $('.map').on('click', onMapClickHandler);
 
 window.onload = function() {
     // similar behavior as an HTTP redirect
-    window.location.replace("http://agungpriambada.blogspot.com");
+    window.location.replace("https://blog.zeneight.xyz");
 }
